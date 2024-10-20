3953b9bd776593c22a30d485fde76d071e2e56b1
Bastien <156309139+GrandeTB@users.noreply.github.com>
2024-10-20 15:16:57 +0200

    Add files via upload

---
+++ sketch_oct19c.ino
@@ -0,0 +1,29 @@
+#include <sparki.h> //include Sparki library
+void setup(){
+}
+ 
+void loop()
+{
+
+  sparki.RGB(100,0,0); // Make the LED maximum Red
+  delay(500); // wait 0.5 seconds (500 milliseconds)
+
+  sparki.RGB(0,100,0); // Make the LED maximum Green
+  delay(500);
+
+  sparki.RGB(0,0,100); // Make the LED maximum Blue
+  delay(500);
+
+  sparki.RGB(RGB_WHITE); // Make the LED white (all colors)
+  delay(500);
+
+  sparki.RGB(RGB_OFF); // Make the LED white (all colors)
+  delay(500);
+  sparki.moveForward();
+  delay(3000);
+  sparki.moveBackward();
+  delay(3000);
+  sparki.moveRight(15);
+  sparki.moveStop();
+  delay(1000);
+}
